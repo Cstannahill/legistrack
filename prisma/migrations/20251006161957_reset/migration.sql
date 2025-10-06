7554fea43ad304db6dadd1b90a0ef0654f014b34
Christian Tannahill <cstannahill10@gmail.com>
2025-10-06 12:10:48 -0500

    prisma migrations sync and reset

--- prisma/migrations/20251006161957_reset/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "public"."NotificationPreference" DROP CONSTRAINT "NotificationPreference_userId_fkey";
-
--- AddForeignKey
-ALTER TABLE "NotificationPreference" ADD CONSTRAINT "NotificationPreference_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
